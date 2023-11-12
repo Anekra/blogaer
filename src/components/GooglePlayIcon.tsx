b551f0aa1ae8b7fc44dab768237a435303cfb0ee
Andika Eka Putra <andika.eka.102@gmail.com>
2023-11-12 08:40:37 +0700

    feature: add GooglePlayIcon component

---
+++ src/components/GooglePlayIcon.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { Icon } from '@iconify/react/dist/iconify.js';
+import React from 'react';
+
+function GooglePlayIcon() {
+  return <Icon icon="mdi:google-play" className="text-xl sm:text-4xl" />;
+}
+
+export default GooglePlayIcon;
