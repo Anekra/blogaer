4849abbb4c8e66288a5468067528f068faee3362
Andika Eka Putra <andika.eka.102@gmail.com>
2023-11-12 08:42:33 +0700

    feature: add StoriesIcon component

---
+++ src/components/StoriesIcon.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import { Icon } from '@iconify/react/dist/iconify.js';
+
+function StoriesIcon() {
+  return <Icon icon="material-symbols:auto-stories" className="text-4xl" />;
+}
+
+export default StoriesIcon;
