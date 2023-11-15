d7bf131c373a26a5bfcf87d99a2a1cc2cdae5937
Andika Eka Putra <andika.eka.102@gmail.com>
2023-11-15 14:06:00 +0700

    feature: add BackBtn component

---
+++ src/components/BackBtn.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { cn } from '@/lib/utils';
+import { Icon } from '@iconify/react/dist/iconify.js';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+export default function BackBtn({className}: {className: string | undefined;}) {
+  const router = useRouter();
+  return(
+    <button className="absolute left-5 top-5 rounded-full text-xl hover:bg-foreground hover:text-background"
+      onClick={() => router.back()}>
+      <Icon icon="mingcute:arrow-left-fill" className={cn(className)} />
+    </button>
+  ) 
+}
