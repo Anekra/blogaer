0347d130d4a9c71a907c16d22b13ab892f04011b
Andika Eka Putra <andika.eka.102@gmail.com>
2023-11-15 14:07:16 +0700

    refactor: sort tailwind classes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <footer className="bottom-0 flex gap-2 w-screen items-center justify-between py-4 px-2 sm:px-12 shadow-[0_-0.5px_2px_#334041] dark:shadow-[0_-0.5px_2px_#ffffff]">
+    <footer className="bottom-0 flex w-screen items-center justify-between gap-2 px-2 py-4 shadow-[0_-0.5px_2px_#334041] dark:shadow-[0_-0.5px_2px_#ffffff] sm:px-12">
       <div className="flex">
-        <button className="flex items-center gap-1 border border-black/20 dark:border-white/20 p-2 rounded-xl hover:shadow-[0_2px_4px_0_rgb(0,0,0,0.9)] dark:hover:shadow-[0_2px_4px_0_rgb(255,255,255,0.9)] transition-transform active:translate-y-[4px] active:shadow-none dark:hover:active:shadow-none">
+        <button className="flex items-center gap-1 rounded-xl border border-black/20 p-2 transition-transform hover:shadow-[0_2px_4px_0_rgb(0,0,0,0.9)] active:translate-y-[4px] active:shadow-none dark:border-white/20 dark:hover:shadow-[0_2px_4px_0_rgb(255,255,255,0.9)] dark:hover:active:shadow-none">
           <GooglePlayIcon />
           <div className="flex flex-col items-start gap-1 leading-none">
             <h3 className="text-[8px] sm:text-[10px]">GET IT ON</h3>
@@ -16,10 +16,14 @@ function Footer() {
           </div>
         </button>
       </div>
-      <div className="flex lg:gap-6 gap-2 text-xs xs:text-sm sm:text-base">
+      <div className="flex gap-2 text-xs xs:text-sm sm:text-base lg:gap-6">
         <p>© Blogaer</p>
-        <Link href="/terms" className="underline text-secondary-foreground">Terms</Link>
-        <Link href="/privacy" className="underline text-secondary-foreground">Privacy</Link>
+        <Link href="/terms" className="text-secondary-foreground underline">
+          Terms
+        </Link>
+        <Link href="/privacy" className="text-secondary-foreground underline">
+          Privacy
+        </Link>
       </div>
     </footer>
   );
