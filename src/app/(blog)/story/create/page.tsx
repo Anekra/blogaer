fa221d9e2f4b9ae0680286f361227d8316715d48
Anekra <andika.eka.102@gmail.com>
2024-03-07 13:36:44 +0700

    feature: add create story page

---
+++ src/app/(blog)/story/create/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CreateStoryForm from '@/lib/components/CreateStoryForm';
+
+export default function CreateStory() {
+  return (
+    <div>
+      <CreateStoryForm />
+    </div>
+  );
+}
