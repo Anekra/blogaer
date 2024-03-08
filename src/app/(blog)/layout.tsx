7539a03229995a4fcba4491e62873cc0acfad3c2
Anekra <andika.eka.102@gmail.com>
2024-03-08 23:20:45 +0700

    refactor: remove user prop

--- src/app/(blog)/layout.tsx
+++ src/app/(blog)/layout.tsx
@@ -1,13 +1,10 @@
 import Navbar from '@/lib/components/Navbar';
 import React from 'react';
-import { auth } from '@/lib/auth';
 
-export default async function BlogLayout({children}: {children: React.ReactNode}) {
-  const session = await auth()
-  
+export default async function BlogLayout({children}: {children: React.ReactNode}) {  
   return (
     <main className="w-screen">
-      <Navbar user={session?.user} />
+      <Navbar />
       {children}
     </main>
   );
