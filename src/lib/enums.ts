c5ff35ac1df51b17737d7eaffceb2402a5f93995
Anekra <andika.eka.102@gmail.com>
2024-03-10 16:34:38 +0700

    Feature: add enums file

---
+++ src/lib/enums.ts
@@ -0,0 +1,59 @@
+export enum WysiwygType {
+  Paragraph = 'paragraph',
+  Heading = 'heading',
+  Code = 'code',
+  Image = 'image',
+  Quote = 'quote',
+  Divider = 'divider',
+  ListBullets = 'bullets',
+  ListNumbers = 'numbers'
+}
+
+export enum WysiwygAlign {
+  Left = 'text-left',
+  Center = 'text-center',
+  Right = 'text-right',
+  Justify = 'text-justify',
+  ImgLeft = 'justify-start',
+  ImgCenter = 'justify-center',
+  ImgRight = 'justify-end'
+}
+
+export enum WysiwygStyle {
+  Bold = 'bold',
+  Italic = 'italic',
+  Underline = 'underline',
+  Strike = 'del',
+  Link = 'link'
+}
+
+export enum WysiwygTypeIcon {
+  Paragraph = 'P',
+  Heading = 'H',
+  Code = '<>',
+  Image = '🖼',
+  Quote = '❛❛',
+  Divider = '⎯',
+  ListBullets = 'bullets',
+  ListNumbers = 'numbers'
+}
+
+export enum WysiwygStyleIcon {
+  Bold = 'strong',
+  Italic = 'em',
+  Underline = 'u',
+  Strike = 'del'
+}
+
+export enum HotKeys {
+  'mod+b' = 'bold',
+  'mod+i' = 'italic',
+  'mod+u' = 'underline',
+  'mod+delete' = 'del'
+}
+
+export enum HeadingSize {
+  Medium = 'text-2xl',
+  Large = 'text-3xl',
+  ExtraLarge = 'text-5xl'
+}
