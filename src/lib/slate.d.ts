ee01ef84b9bb617a388bfe56a31cdb77a726cae5
Anekra <andika.eka.102@gmail.com>
2024-03-13 23:32:16 +0700

    Feature: add slate js types for typescript

---
+++ src/lib/slate.d.ts
@@ -0,0 +1,23 @@
+import { Extension } from '@uiw/react-codemirror';
+import { BaseEditor } from 'slate';
+import { HistoryEditor } from 'slate-history';
+import { ReactEditor } from 'slate-react';
+import { WysiwygType } from './enums';
+import { langs } from '@uiw/codemirror-extensions-langs';
+
+type CustomText = { text: string; [key: string]: any };
+type CustomElement = {
+  type: string;
+  children: CustomText[];
+  align?: string;
+  url?: string;
+  [key?: string]: any;
+};
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor & HistoryEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
+}
