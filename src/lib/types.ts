fc2e31d27765bc9c2f201792d052476b848c49ac
Anekra <andika.eka.102@gmail.com>
2024-03-13 23:34:54 +0700

    Refactor: add refresh property to userResponse.data, add new SlateEditor and ImgDimensions type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,7 @@
+import { BaseEditor } from 'slate';
+import { HistoryEditor } from 'slate-history';
+import { ReactEditor } from 'slate-react';
+
 export type UserResponse = {
   status: string;
   message: string;
@@ -6,6 +10,7 @@ export type UserResponse = {
     email: string;
     role: string;
     token: string;
+    refresh: string;
   };
 };
 
@@ -15,8 +20,14 @@ export type Token = {
   role: string;
 };
 
-export type User = {
-  username: string;
-  email: string | null | undefined;
-  role: string;
-} | undefined;
+export type User =
+  | {
+      username: string;
+      email: string | null | undefined;
+      role: string;
+    }
+  | undefined;
+
+export type SlateEditor = BaseEditor & ReactEditor & HistoryEditor;
+
+export type ImgDimensions = { width: number, height: number};
