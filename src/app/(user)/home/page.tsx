adfabacc26636e5b55bb9c15ecd54354957071dd
anekra <andika.eka.102@gmail.com>
2024-08-16 20:54:45 +0700

    Change used component.

--- src/app/(user)/home/page.tsx
+++ src/app/(user)/home/page.tsx
@@ -1,10 +1,10 @@
-import PostGridItemB from '@/lib/components/item/PostGridItemB';
+import PostItemsHolderA from '@/lib/components/post/PostItemsHolderA';
 import React from 'react';
 
 export default function HomePage() {
   return (
     <main className="flex flex-wrap gap-6 p-6 pb-20 pt-[92px]">
-      <PostGridItemB />
+      <PostItemsHolderA />
     </main>
   );
 }
