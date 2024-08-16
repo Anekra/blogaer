263a5b8b9e6c979c3a9a6605fbc030ce8f82794d
anekra <andika.eka.102@gmail.com>
2024-08-16 20:39:40 +0700

    Refactor: rename Logo to LogoIcon.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,13 +1,13 @@
-import BackBtn from "@/lib/components/BackBtn";
-import LoginForm from "@/lib/components/LoginForm";
-import Logo from "@/lib/components/Logo";
+import BackBtn from '@/lib/components/buttons/BackBtn';
+import LoginForm from '@/lib/components/forms/LoginForm';
+import LogoIcon from '@/lib/components/icons/LogoIcon';
 
 export default async function Login() {
   return (
     <main className="radial-background relative flex min-h-screen items-center justify-center py-6">
       <BackBtn className="md:text-5xl" />
       <div className="absolute">
-        <Logo className="h-[150px] w-[180px] text-primary-foreground brightness-75" />
+        <LogoIcon className="h-[150px] w-[180px] text-primary-foreground brightness-75" />
       </div>
       <div className="glass-form flex w-[400px] flex-col gap-3 px-10 py-6">
         <div className="w-full py-2">
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,13 +1,13 @@
-import BackBtn from '@/lib/components/BackBtn';
-import RegisterForm from '@/lib/components/RegisterForm';
-import Logo from '@/lib/components/Logo';
+import BackBtn from '@/lib/components/buttons/BackBtn';
+import RegisterForm from '@/lib/components/forms/RegisterForm';
+import LogoIcon from '@/lib/components/icons/LogoIcon';
 
 export default function Register() {
   return (
     <main className="radial-background relative flex min-h-screen items-center justify-center py-6">
       <BackBtn className="md:text-5xl" />
       <div className="absolute">
-        <Logo className="h-[150px] w-[180px] text-primary-foreground brightness-75" />
+        <LogoIcon className="h-[150px] w-[180px] text-primary-foreground brightness-75" />
       </div>
       <div className="glass-form flex w-[400px] flex-col gap-3 px-10 py-6">
         <div className="w-full">
